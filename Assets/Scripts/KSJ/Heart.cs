f26b06afc7eae8e2cbe4fd421de28e493515a5ab
PotassiumCyanide <szziny00@gmail.com>
2024-05-09 23:49:35 +0900

    Add Heart(hp) UI

---
+++ Assets/Scripts/KSJ/Heart.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Heart : MonoBehaviour
+{
+    int maxheart = 5;
+    int heart;
+
+    public GameObject heartUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        heart = maxheart;
+        float starting_x = (maxheart-1) * -0.25f;
+        for (int i = 0; i < maxheart; i++)
+        {
+            Instantiate(heartUI, new Vector3(starting_x, -4.5f, 0), Quaternion.identity, GameObject.Find("HeartGroup").transform);
+            starting_x += 0.5f;
+        }
+    }
+
+    public void HeartBreak() 
+    {
+        Destroy(transform.GetChild(heart - 1).gameObject);
+        heart--;
+        if (heart <= 0) {
+            //대충 게임오버
+        }
+    }
+}
